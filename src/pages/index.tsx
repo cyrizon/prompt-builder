c34ff13b0aa3d028d360edebf9d73c299118fb3e
Cyrizon <56123717+cyrizon@users.noreply.github.com>
2025-10-16 00:14:12 +0200

    Basic architecture

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      Hello World
+    </div>
+  );
+};
+
+export default HomePage;
