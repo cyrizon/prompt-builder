1b7494503e37cc1f2a602f9dd6c863614dad2fdd
Cyrizon <56123717+cyrizon@users.noreply.github.com>
2025-11-09 21:35:05 +0100

    update build

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/prompt-builder',
@@ -8,6 +9,7 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  distDir: 'build',
 };
 
 export default nextConfig;
