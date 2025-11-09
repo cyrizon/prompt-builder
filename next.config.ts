d8772d1d76e8dc77c729ccf273ba0c3546ae2298
Cyrizon <56123717+cyrizon@users.noreply.github.com>
2025-11-09 20:11:41 +0100

    Update for gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/prompt-builder',
+  assetPrefix: '/prompt-builder',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
